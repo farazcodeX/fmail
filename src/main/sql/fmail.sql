beb40e16196ce666e85f9aa021e6b7fc182a6355
faraz <farazn8564@gmail.com>
2025-08-11 12:24:08 +0330

    first creation setup + main + sql files

---
+++ src/main/sql/fmail.sql
@@ -0,0 +1,11 @@
+USE Fmail;
+CREATE TABLE fmails (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    origin_id INT NOT NULL,
+    subject NVARCHAR(255) NOT NULL ,
+    body NVARCHAR(255) NOT NULL ,
+    date DATETIME,
+    code NVARCHAR(6) NOT NULL UNIQUE ,
+
+    FOREIGN KEY (origin_id) REFERENCES users(id)
+);
---
+++ src/main/sql/user.sql
@@ -0,0 +1,7 @@
+USE Fmail;
+CREATE TABLE users (
+    id INT  AUTO_INCREMENT PRIMARY KEY ,
+    name NVARCHAR(50) NOT NULL ,
+    fmail NVARCHAR(50) NOT NULL UNIQUE ,
+    password NVARCHAR(50) NOT NULL
+);
